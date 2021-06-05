253716d7e676ec0fdedc45bf1e9e941f0a23822c
Koichi Shiraishi <zchee.io@gmail.com>
2021-06-05 12:36:19 +0900

    tools: use go-darwin.dev/tools/cmd/asmvet
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- tools/go.mod
+++ tools/go.mod
@@ -2,4 +2,8 @@ module go-darwin.dev/mach/tools
 
 go 1.17
 
-require github.com/klauspost/asmfmt v1.3.0
+require (
+	github.com/klauspost/asmfmt v1.3.0
+	go-darwin.dev/tools v0.0.0-20210605033523-1812d5b2a8e0
+	golang.org/x/tools v0.1.3-0.20210604203405-1225b6f53f67 // indirect
+)
--- tools/tools.go
+++ tools/tools.go
@@ -1,12 +1,12 @@
 // SPDX-FileCopyrightText: Copyright 2021 The go-darwin Authors
 // SPDX-License-Identifier: BSD-3-Clause
 
-//go:build tools
-// +build tools
+//go:build darwin && amd64 && gc && tools
+// +build darwin,amd64,gc,tools
 
-// Package tools manages tools using during development.
 package tools
 
 import (
 	_ "github.com/klauspost/asmfmt/cmd/asmfmt"
+	_ "go-darwin.dev/tools/cmd/asmvet"
 )
