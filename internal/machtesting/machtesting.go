4571b1d0de949c3c17a9e371af824681aeed9c40
Koichi Shiraishi <zchee.io@gmail.com>
2022-05-09 05:08:08 +0900

    internal/machtesting: add machtesting package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ internal/machtesting/machtesting.go
@@ -0,0 +1,17 @@
+// Copyright 2022 The Go Darwin Authors
+// SPDX-License-Identifier: BSD-3-Clause
+
+//go:build darwin && amd64
+// +build darwin,amd64
+
+package machtesting
+
+/*
+#include <mach/mach.h>
+*/
+import "C"
+
+func TaskSelfTrap() uint32 {
+	ret := C.task_self_trap()
+	return uint32(ret)
+}
