d95fc9fb2b69bbbbd595a030dbaf13bff8604146
Koichi Shiraishi <zchee.io@gmail.com>
2022-05-09 20:18:05 +0900

    mach: keep stack aligned
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- mach.s
+++ mach.s
@@ -8,30 +8,42 @@
 
 // func machReplyPort() (ret uint32)
 TEXT ·machReplyPort(SB), NOSPLIT, $0
-	MOVL $(0x1000000+26), AX // mach_reply_port
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+26), AX // mach_reply_port
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func threadSelfTrap() (ret uint32)
 TEXT ·threadSelfTrap(SB), NOSPLIT, $0
-	MOVL $(0x1000000+27), AX // thread_self_trap
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+27), AX // thread_self_trap
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func taskSelfTrap() (ret uint32)
 TEXT ·taskSelfTrap(SB), NOSPLIT, $0
-	MOVL $(0x1000000+28), AX // task_self_trap
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+28), AX // task_self_trap
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func hostSelfTrap() (ret uint32)
 TEXT ·hostSelfTrap(SB), NOSPLIT, $0
-	MOVL $(0x1000000+29), AX // host_self_trap
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+29), AX // host_self_trap
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func machMsgTrap(msg *MachMsgHeader, option MachMsgOption, sendSize, rcvSize MachMsgSize, rcvName MachPortName, timeout MachMsgTimeout, notify MachPortName) (ret MachMsgReturn)
@@ -52,35 +64,50 @@ TEXT ·machMsgTrap(SB), NOSPLIT, $0
 
 // func threadGetSpecialReplyPort() (ret MachPortName)
 TEXT ·threadGetSpecialReplyPort(SB), NOSPLIT, $0
-	MOVL $(0x1000000+50), AX // thread_get_special_reply_port
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+50), AX // thread_get_special_reply_port
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func pfzExit() (ret sys.KernReturn)
 TEXT ·pfzExit(SB), NOSPLIT, $0
-	MOVL $(0x1000000+58), AX // pfz_exit
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+58), AX // pfz_exit
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
 
 // func swtchPri() (ret bool)
 TEXT ·swtchPri(SB), NOSPLIT, $0
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
 	MOVL  $(0x1000000+59), AX // swtch_pri
 	SYSCALL
 	SETEQ ret+0(FP)
+	POPQ  BP
 	RET
 
 // func swtch() (ret bool)
 TEXT ·swtch(SB), NOSPLIT, $0
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
 	MOVL  $(0x1000000+60), AX // swtch
 	SYSCALL
 	SETEQ ret+0(FP)
+	POPQ  BP
 	RET
 
 // func mkTimerCreateTrap() (ret MachPortName)
 TEXT ·mkTimerCreateTrap(SB), NOSPLIT, $0
-	MOVL $(0x1000000+91), AX // mk_timer_create_trap
+	PUSHQ BP                  // make a frame, keep stack aligned
+	MOVQ  SP, BP
+	MOVL  $(0x1000000+91), AX // mk_timer_create_trap
 	SYSCALL
-	MOVL AX, ret+0(FP)
+	MOVL  AX, ret+0(FP)
+	POPQ  BP
 	RET
